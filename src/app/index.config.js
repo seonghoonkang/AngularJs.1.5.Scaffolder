48128982862ee1bbd4b64d0f6375580f477928e7
seonghoonkang <kangpual@naver.com>
2018-09-05 10:19:13 +0900

    URL 해시 값 삭제
    HTML5 mode  true 설정

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,32 +1,37 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('hbcManager')
-    .config(toastrConfig)
-    .config(translateConfig);
+    angular
+        .module('hbcManager')
+        .config(toastrConfig)
+        .config(translateConfig)
+        .config(locationConfig);
 
-  /** @ngInject */
-  function toastrConfig($logProvider, toastrConfig) {
-    // Enable log
-    $logProvider.debugEnabled(true);
+    /** @ngInject */
+    function toastrConfig($logProvider, toastrConfig) {
+        // Enable log
+        $logProvider.debugEnabled(true);
 
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = true;
-    toastrConfig.progressBar = true;
-  }
+        // Set options third-party lib
+        toastrConfig.allowHtml = true;
+        toastrConfig.timeOut = 3000;
+        toastrConfig.positionClass = 'toast-top-right';
+        toastrConfig.preventDuplicates = true;
+        toastrConfig.progressBar = true;
+    }
 
-  /** @ngInject */
-  function translateConfig($translateProvider) {
-    $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
-    $translateProvider.useStaticFilesLoader({
-      prefix: '/assets/i18n/lang-',
-      suffix: '.json'
-    });
-    $translateProvider.preferredLanguage('ko');
-  }
+    /** @ngInject */
+    function translateConfig($translateProvider) {
+        $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+        $translateProvider.useStaticFilesLoader({
+            prefix: '/assets/i18n/lang-',
+            suffix: '.json'
+        });
+        $translateProvider.preferredLanguage('ko');
+    }
 
+    /** @ngInject */
+    function locationConfig($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }
 })();
